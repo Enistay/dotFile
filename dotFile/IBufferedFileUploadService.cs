03fc79eda96a2c392ea08bacd5ae17be84f96f6b
Enistay Pinheiro Andrade <n.stay@hotmail.com>
2023-06-09 07:47:12 -0300

    Add project files.

---
+++ dotFile/IBufferedFileUploadService.cs
@@ -0,0 +1,7 @@
+﻿namespace dotFile
+{
+    public interface IBufferedFileUploadService
+    {
+        Task<bool> UploadFile(IFormFile file);
+    }
+}
---
+++ dotFile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotFile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
